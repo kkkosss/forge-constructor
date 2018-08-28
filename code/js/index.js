704584e996f2007382e206c3c7b259a4a870159e
Alexey Pazhiltsev <alexey.pazhiltsev@autodesk.com>
2018-08-28 16:52:00 +0300

    вв

--- code/js/index.js
+++ code/js/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import Header from'./components/Header'
 
 ReactDOM.render(
-  <div>Hello world</div>, 
+  <Header />, 
   document.getElementById('fieldToShow')
 );
