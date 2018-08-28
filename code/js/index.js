5aecedcecd84c1d4eeac3702f6c9d339c1dbd41d
Alexey Pazhiltsev <alexey.pazhiltsev@autodesk.com>
2018-08-28 12:37:41 +0300

    work

--- code/js/index.js
+++ code/js/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <h2>Здесь может быть любой текст</h2>, 
+  <div>Hello world</div>, 
   document.getElementById('fieldToShow')
 );
