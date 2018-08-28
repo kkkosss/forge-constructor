59287fab4096a7c435c588f3e46f8934b2124ab5
Maxim Demyanenko <maxim.demyanenko@autodesk.com>
2018-08-28 17:09:04 +0300

    test

--- code/js/index.js
+++ code/js/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import Header from'./components/Header'
+import Footer from './components/Footer'
+import '../css/index.scss'
 
 ReactDOM.render(
-  <Header />, 
+  <div>
+  <Header />
+  <Footer/>
+  </div>
+  , 
   document.getElementById('fieldToShow')
 );
